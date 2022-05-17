29985852667db5f3b634509a3ebb2fd5b2217244
soyambhu <85630358+soyambhu@users.noreply.github.com>
2022-05-17 16:04:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 function preload() {
-	mario_gameover = loadSound("gameover.wav");
-	mario_jump = loadSound("jump.wav");
-	mario_coin = loadSound("coin.wav");
-	mario_kick = loadSound("kick.wav");
-	mario_die = loadSound("mariodie.wav");
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
-}A
+}
 
 function setup() {
 	canvas = createCanvas(1240,336);
